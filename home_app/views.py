aac96d9dfe291c09f24b4c52247b9f277e4ce13c
Reza savari <rezasavari833@gmail.com>
2025-03-19 16:27:40 +0330

    add dynamic  recent post to index template

--- home_app/views.py
+++ home_app/views.py
@@ -5,5 +5,6 @@ from blog_app.models import Article
 def home(request):
     # articles = Article.objects.all()      /// Manager
     # articles = Article.objects.filter(status = True)    
-    articles = Article.objects.published()  
-    return render(request , "home_app/index.html" , {'articles' : articles})
+    articles = Article.objects.published()
+    resent_articles = Article.objects.all().order_by('-created')
+    return render(request , "home_app/index.html" , {'articles' : articles , 'resent_articles':resent_articles})
