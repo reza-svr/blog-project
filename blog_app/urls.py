57b23d54f821afbada4d554fdb3bc20b1775cfcb
Reza savari <rezasavari833@gmail.com>
2025-03-21 17:29:59 +0330

    add search to sidebar (views and urls )

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -6,5 +6,6 @@ app_name = "blog"
 urlpatterns = [
     path('detail/<slug:slug>',views.post_detail , name="article_detail"),
     path('list',views.post_list,name="post_list"),
-    path('category/<int:pk>',views.category_post_list , name="category_post_list")
+    path('category/<int:pk>',views.category_post_list , name="category_post_list"),
+    path('search/',views.search , name="search_post"),
 ]
--- blog_app/views.py
+++ blog_app/views.py
@@ -24,3 +24,11 @@ def category_post_list(request , pk=None):
     category = get_object_or_404(Category, id=pk)
     articles = category.article_set.all()
     return render(request , "blog_app/post-list.html" , {'articles' : articles})
+
+def search(request):
+    q = request.GET.get('q')
+    articles = Article.objects.filter(title__icontains = q)
+    page_number = request.GET.get('page')
+    paginator = Paginator(articles , 1)
+    object_list = paginator.get_page(page_number)
+    return render(request , "blog_app/post-list.html" ,{'articles' : object_list} )
